4c0fff14dfb65947dbbd9a04672f7bdb4952f6e1
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-07-17 10:23:04 -0600

    Login de usuario con firebase Authentication

--- app/src/main/java/com/michael/uberclone/SelectOptionAuthActivity.java
+++ app/src/main/java/com/michael/uberclone/SelectOptionAuthActivity.java
@@ -2,12 +2,17 @@ package com.michael.uberclone;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 import androidx.appcompat.widget.Toolbar;
 
 public class SelectOptionAuthActivity extends AppCompatActivity {
 
     public Toolbar mToolbar;
+    public Button  mButtonGoToLogin;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,5 +22,18 @@ public class SelectOptionAuthActivity extends AppCompatActivity {
         setSupportActionBar(mToolbar);
         getSupportActionBar().setTitle("Seleccionar opción");
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mButtonGoToLogin = findViewById(R.id.btnGoToLogin);
+        mButtonGoToLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                goToLogin();
+            }
+        });
+    }
+
+    private void goToLogin() {
+        Intent intent =  new Intent(SelectOptionAuthActivity.this, LoginActivity.class);
+        startActivity(intent);
     }
 }
