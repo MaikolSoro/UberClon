ad3748ece754cedbbcf52a454ddd2515e4039b40
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-07-16 19:54:45 -0600

    Crear toolbar

--- app/src/main/java/com/michael/uberclone/SelectOptionAuthActivity.java
+++ app/src/main/java/com/michael/uberclone/SelectOptionAuthActivity.java
@@ -3,12 +3,19 @@ package com.michael.uberclone;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import androidx.appcompat.widget.Toolbar;
 
 public class SelectOptionAuthActivity extends AppCompatActivity {
 
+    public Toolbar mToolbar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_option_auth);
+
+        mToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Seleccionar opción");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
 }
