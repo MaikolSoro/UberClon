1aa6f728e0d079bbcc99690d687ed8f3e373528a
Maikol Soro <maikolsoro.z1998@gmail.com>
2020-07-17 11:09:46 -0600

    Implemetación del progress Dialog

--- app/src/main/java/com/michael/uberclone/LoginActivity.java
+++ app/src/main/java/com/michael/uberclone/LoginActivity.java
@@ -3,6 +3,7 @@ package com.michael.uberclone;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,8 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import dmax.dialog.SpotsDialog;
+
 public class LoginActivity extends AppCompatActivity {
 
     private TextInputEditText mTextInputEmail;
@@ -24,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
 
     public FirebaseAuth mAuth;
     public DatabaseReference mDatabase;
+    AlertDialog mDialog;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -36,6 +40,7 @@ public class LoginActivity extends AppCompatActivity {
         mAuth = FirebaseAuth.getInstance();
         mDatabase = FirebaseDatabase.getInstance().getReference();
 
+        mDialog = new SpotsDialog.Builder().setContext(LoginActivity.this).setMessage("Espere un momento").build();
         mButtonLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -50,6 +55,7 @@ public class LoginActivity extends AppCompatActivity {
 
         if(!email.isEmpty() && !password.isEmpty()) {
             if (password.length() >= 6) {
+                mDialog.show();
                 mAuth.signInWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
                     @Override
                     public void onComplete(@NonNull Task<AuthResult> task) {
@@ -59,6 +65,7 @@ public class LoginActivity extends AppCompatActivity {
                         else {
                             Toast.makeText(LoginActivity.this, "El email o la contraseña son incorrectos ", Toast.LENGTH_SHORT).show();
                         }
+                        mDialog.dismiss();
                     }
                 });
             } else{
